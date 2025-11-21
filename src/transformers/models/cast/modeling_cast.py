35017e1f5816425dada398a0a251edc5642aeb1e
Sarin Eapen Chandy <sarin1991@gmail.com>
2025-11-21 11:00:30 -0600

    Using liger kernel for RMS norm

--- src/transformers/models/cast/modeling_cast.py
+++ src/transformers/models/cast/modeling_cast.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from dataclasses import dataclass
+from liger_kernel.transformers import LigerRMSNorm
 
 from ...activations import ACT2FN
 from ...cache_utils import Cache, DynamicCache, SlidingWindowCache, StaticCache
@@ -171,24 +172,17 @@ class CastAttention(nn.Module):
         return attn_output, attn_weights
 
 
-class CastRMSNorm(nn.Module):
+class CastRMSNorm(LigerRMSNorm):
     def __init__(self, hidden_size, eps=1e-6):
-        """
-        CastRMSNorm is equivalent to T5LayerNorm
-        """
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
-        self.variance_epsilon = eps
-
-    def forward(self, hidden_states):
-        input_dtype = hidden_states.dtype
-        hidden_states = hidden_states.to(torch.float32)
-        variance = hidden_states.pow(2).mean(-1, keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
-
-    def extra_repr(self):
-        return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
+        super().__init__(
+            hidden_size=hidden_size,
+            eps=eps,
+            offset=0.0,
+            casting_mode="llama",
+            init_fn="ones",
+            in_place=True,
+            row_mode=None,
+        )
 
 
 class CastDecoderLayer(nn.Module):
